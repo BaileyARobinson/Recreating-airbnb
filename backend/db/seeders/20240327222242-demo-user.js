4d122c57503b2a344d30ab98d04e7f28707c8c6a
Bailey <robinson.bailey.a@gmail.com>
2024-03-27 15:43:24 -0700

    Migration, Model, and Seeder Complete

---
+++ backend/db/seeders/20240327222242-demo-user.js
@@ -0,0 +1,51 @@
+'use strict';
+const { User } = require('../models')
+const  bcrypt  = require('bcryptjs')
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await User.bulkCreate([
+
+    {
+      email: "myemail@user.com",
+      username: 'it-me',
+      hashedPassword: bcrypt.hashSync('password')
+    }, {
+      email: 'username@myemail.com',
+      username: 'itstillme',
+      hashedPassword: bcrypt.hashSync('password2')
+    }
+
+   ], { vaidate: true });
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]:  ['it-me', 'itstillme',] }
+    }, {});
+  }
+};
