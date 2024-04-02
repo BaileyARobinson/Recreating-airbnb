35c33323c030a4d78bbc2f8652e27e646237b180
Bailey <robinson.bailey.a@gmail.com>
2024-04-02 12:21:42 -0700

    update evn_variable to new database

--- backend/config/database.js
+++ backend/config/database.js
@@ -10,7 +10,7 @@ module.exports = {
     typeValidation: true
   },
   production: {
-    use_env_variable: 'postgres://app_academy_projects_3mhh_user:YNdeyKhqpMUHGQHQO54G55zZZC1aZOOl@dpg-co2vh0sf7o1s738a4hk0-a/app_academy_projects_3mhh',
+    use_env_variable: 'postgres://all_app_academy_projects_7my6_user:k7dOIrZNoAnn9Iboef8Xetu5DmqxOpFd@dpg-co64eqol5elc73abda80-a.oregon-postgres.render.com/all_app_academy_projects_7my6',
     dialect: 'postgres',
     seederStorage: 'sequelize',
     dialectOptions: {
