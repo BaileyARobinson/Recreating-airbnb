2c98e47a229d4674f0fdf000116046b39a198a2f
Bailey <robinson.bailey.a@gmail.com>
2024-04-02 12:32:13 -0700

    fix dev_env

--- backend/config/database.js
+++ backend/config/database.js
@@ -10,7 +10,7 @@ module.exports = {
     typeValidation: true
   },
   production: {
-    use_env_variable: 'postgres://all_app_academy_projects_7my6_user:k7dOIrZNoAnn9Iboef8Xetu5DmqxOpFd@dpg-co64eqol5elc73abda80-a.oregon-postgres.render.com/all_app_academy_projects_7my6',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
     dialectOptions: {
